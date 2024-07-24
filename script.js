d3a567e9d19b92c62b805a5a4ea53c91b8930d54
Cesarwrze <137361385+Cesarwrze@users.noreply.github.com>
2024-07-24 11:22:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ okButton.addEventListener('click', function(event) {
 
 window.addEventListener('pageshow', function(event) {
     if (event.persisted) {
-      if (!loadingButton.classList.contains('d-none')) {
+      if (okButton.classList.contains('d-none')) {
         loadingButton.classList.add('d-none');
+        okButton.classList.remove('d-none');
       }
     }
 });
